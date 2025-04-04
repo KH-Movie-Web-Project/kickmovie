8d2735cf1bad3499152e07579fdc2eb75f86c84b
euihyunee <lobil7782@gmail.com>
2025-04-04 09:14:19 +0900

    [init] spring boot project setup

---
+++ kickmovie/src/main/java/kh/gangnam/kickmovie/KickmovieApplication.java
@@ -0,0 +1,13 @@
+package kh.gangnam.kickmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickmovieApplication.class, args);
+	}
+
+}
---
+++ kickmovie/src/test/java/kh/gangnam/kickmovie/KickmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.gangnam.kickmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
