c9b6ba52b285f7eb8cf598d7aa44d58b737ce429
Swhite <nakid21@naver.com>
2025-04-07 18:41:12 +0900

    [test] git branch test

--- kickmovie/src/main/java/kh/gangnam/kickmovie/components/api/ActorListComponent.java
+++ kickmovie/src/main/java/kh/gangnam/kickmovie/components/api/ActorListComponent.java
@@ -16,6 +16,7 @@ public class ActorListComponent{
     public ResponseEntity<?> fetchData(String url, HttpHeaders headers) {
         // TODO API 호출하여 배우 리스트 응답 데이터 DTO 반환하기
         // URL과 Header는 Service에서 구현
+        System.out.println("test");
         return null;
     }
 }
