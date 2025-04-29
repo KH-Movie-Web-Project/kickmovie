9e75c113b5a5e685ba8e20572cf8c3206a762f29
euihyunee <lobil7782@gmail.com>
2025-04-29 19:14:02 +0900

    [feat] 현재 상영 중인 영화 리스트 반환 엔드포인트 개발

---
+++ kickmovie/src/main/java/kh/gangnam/kickmovie/dto/Dates.java
@@ -0,0 +1,13 @@
+package kh.gangnam.kickmovie.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Dates {
+    private String maximum;
+    private String minimum;
+}
